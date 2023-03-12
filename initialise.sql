43617da8542f0c6f61e3fb11a16a46c49dd67cfa
saptarshidec <saptarshi_dechaudhury@outlook.com>
2023-03-12 20:55:02 +0530

    Removed inconsistency

--- initialise.sql
+++ initialise.sql
@@ -1,9 +1,9 @@
 INSERT INTO `Admin` VALUES('Albert','pbkdf2_sha256$390000$CC6bPIJFw3dilkprl4svkv$7UyqQ/10jn/XnqPUAfcfI0F/8LSD6jtGba8blDVkoJY=','Albert Einstein','123 Rhinestrasse, Berlin');
 
-INSERT INTO `Room` VALUES(0, False);
-INSERT INTO `Room` VALUES(0, False);
 INSERT INTO `Room` VALUES(0, True);
-INSERT INTO `Room` VALUES(0, False);
+INSERT INTO `Room` VALUES(0, True);
+INSERT INTO `Room` VALUES(0, True);
+INSERT INTO `Room` VALUES(0, True);
 INSERT INTO `Room` VALUES(0, True);
 INSERT INTO `Room` VALUES(0, True);
 INSERT INTO `Room` VALUES(0, True);
